5b31e0a216072bd85b02714bf081bfbc17e7fd45
rvndae <xshen803@gmail.com>
2025-10-28 20:29:37 +0800

    announcement inbox and sentbox

--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -3,8 +3,8 @@ import AppLayout from "../layout/layout";
 import Dashboard from "../pages/dashboard/dashboard";
 import EmployeePage from "../pages/employees/components/employeePage";
 import Login from "../login";
-
-
+import Inbox from "../pages/announcement/inbox";
+import Sentbox from "../pages/announcement/sentbox";
 
 
 
@@ -17,7 +17,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route element={<AppLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/employees" element={<EmployeePage />} />   
+          <Route path="/employees" element={<EmployeePage />} /> 
+          <Route path="/inbox" element={<Inbox />} /> 
+          <Route path="/sentbox" element={<Sentbox />} />     
         </Route>
       </Routes>
     </Router>
