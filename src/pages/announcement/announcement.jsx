eb99144731336a921243a0be67d83ba1d496f10e
Mamapoco2 <johnspearrow@gmail.com>
2025-10-29 09:04:23 +0800

    chore: drop announcement and other login page

--- src/pages/announcement/announcement.jsx
+++
@@ -1,7 +0,0 @@
-export default function AnnouncementPage() {
-  return (
-    <div className="flex flex-1 items-center justify-center p-4">
-      <h1 className="text-2xl font-bold">Announcement Page</h1>
-    </div>
-  );
-}
