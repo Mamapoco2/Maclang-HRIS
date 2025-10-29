7bdf32bc84f8fd9f4462c45d35cc06063ef35ff4
Mamapoco2 <johnspearrow@gmail.com>
2025-10-29 09:05:03 +0800

    chore: added pages and fix authService

---
+++ src/pages/announcement/Forward/forward.jsx
@@ -0,0 +1,7 @@
+export default function forwardPage() {
+  return (
+    <div className="flex flex-1 items-center justify-center p-4">
+      <h1 className="text-2xl font-bold">Forward Page</h1>
+    </div>
+  );
+}
