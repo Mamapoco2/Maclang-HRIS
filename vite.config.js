33e1e3c8419a2e0b7611cac271b3fa5b6f7e8a36
Mamapoco2 <johnspearrow@gmail.com>
2026-07-31 14:55:09 +0800

    feat: add basic SSL support and update environment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   darkMode: "class",
-  plugins: [react(), tailwindcss()],
+
+  plugins: [
+    react(),
+    tailwindcss(),
+    basicSsl(),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
-    host: true, // exposes to your local network
+    host: "0.0.0.0",
+    https: true,
   },
 });
